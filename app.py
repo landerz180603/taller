eaeb196b20a932fa973757248fb3a66af4c64d18
landerz180603 <paulandresgalloerazo@gmail.com>
2026-06-05 14:07:23 -0500

    correcion y añadir puerto de salida para docker

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
 inventario = []
 
-def agregar_producto(nombre):
-    inventario.append(nombre)
-    return f"Producto '{nombre}' agregado."
+@app.route("/")
+def home():
+    return "API de Inventario funcionando 🚀"
 
+@app.route("/productos", methods=["GET"])
+def obtener_productos():
+    return jsonify(inventario)
+
+@app.route("/productos", methods=["POST"])
+def agregar_producto():
+    data = request.get_json()
+    inventario.append(data["nombre"])
+    return jsonify({"mensaje": "Producto agregado", "inventario": inventario})
+
+@app.route("/productos/<nombre>", methods=["DELETE"])
 def eliminar_producto(nombre):
     if nombre in inventario:
         inventario.remove(nombre)
-        return f"Producto '{nombre}' eliminado."
-    return "Producto no encontrado."
+        return jsonify({"mensaje": "Eliminado", "inventario": inventario})
+    return jsonify({"error": "No encontrado"}), 404
 
 if __name__ == "__main__":
-    print("=== Sistema de Inventario ===")
-
-    agregar_producto("Laptop")
-    agregar_producto("Mouse")
-
-    print("Inventario actual:")
-    print(inventario)
-
-    eliminar_producto("Mouse")
-
-    print("Inventario después de eliminar:")
-    print(inventario)
+    app.run(host="0.0.0.0", port=5000)
